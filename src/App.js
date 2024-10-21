e6d59bfa93217b09ba44dc486e8e807d672ec0fe
Pauline P. Narvas <pauline@paulinenarvas.com>
2024-10-21 16:17:57 +0000

    remove api

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Figure from "./Figure";
 import Why from "./Why";
 
 function App() {
-  const [inspiringfigures, setInspiringfigures] = useState([]);
-  console.log(inspiringfigures);
-
-  if (inspiringfigures.length === 0) {
-    fetch("https://wp.pawlean.com/wp-json/wp/v2/posts?categories=50&per_page=100")
-      .then(response => {
-        return response.json();
-      })
-      .then(posts => {
-        const extract = post => ({
-          title: post.title.rendered,
-          excerpt: post.excerpt.rendered,
-          link: post.link,
-          image: post.jetpack_featured_media_url
-        });
-        const extracted = posts.map(extract);
-
-        return setInspiringfigures(extracted);
-      });
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         <img src="./IF.png" className="logo" alt="IF" />
         <Why />
 
-        <div className="container">
-          {inspiringfigures.map(figure => (
-            <Figure
-              excerpt={figure.excerpt}
-              link={figure.link}
-              title={figure.title}
-              image={figure.image}
-            />
-          ))}
-        </div>
+     
 
         <div className="footer">
           <a
